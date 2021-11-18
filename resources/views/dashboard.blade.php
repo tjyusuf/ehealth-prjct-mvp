f4944f06bce98cb523d8c4aef13070fccdc2ef5f
tjyusuf <tjyusuf@Yahoo.com>
2021-11-18 10:23:58 +0000

    added dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,28 +2,6 @@
 
 @section('content')
     <div>
-
-        <p class="my-2 text-left text-md font-bold text-blue-800">Upcoming Appointments</p>
-        <div class="w-full bg-blue-400 rounded-lg text-white p-5 md:px-10 ">
-            <div class="flex flex-row">
-            <div class="flex-initial">
-                <img alt="" class="w-12 h-12 rounded-full ring-2 ring-offset-4 ring-blue-500 ring-offset-blue-100" src="https://source.unsplash.com/41x41/?portrait">
-            </div>           
-            <h1 class="ml-5 flex-grow">
-                <p class="text-xl font-bold ">Dr Sani Ahmad</p>
-                <p>Dental Specialist</p>
-            </h1>      
-            </div>  
-            <hr class="border-blue-300 my-2"/>
-            <div class="flex flex-row py-2 px-3 bg-blue-500 rounded-lg text-blue-100">
-            <p class="flex-1 text-center">
-                <i class="fa fa-calendar"></i> Wed, 17th Nov
-            </p>
-            <p class="flex-1 text-center">
-                <i class="fa fa-clock"></i> 5:30pm - 6:00pm
-            </p>
-            </div>
-        </div>    
-
+        <script src="https://gist.github.com/tjyusuf/fde5935bc30ac480d45f1a7e612519f3.js"></script>   
     </div>
 @endsection
