578ede67671e068334c01fdc0479bb883943a62d
tjyusuf <tjyusuf@Yahoo.com>
2021-11-18 12:35:18 +0000

    editting mobile nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
 
         <!-- Mobile navigation menu -->
         <div class="md:hidden fixed bottom-1 w-full p-5 px-6 flex items-center justify-between bg-white shadow-lg text-gray-500 rounded-md cursor-pointer">
-            <div class="flex flex-col items-center {{ request()->routeIs('dashboard') }} transition ease-in duration-200 hover:text-blue-400 ">
+            <div class="flex flex-col items-center {{ request()->routeIs('dashboard') ? 'text-blue-400' : '' }} transition ease-in duration-200 hover:text-blue-400 ">
                 <i class="fa fa-home"></i>
             </div>
             <div class="flex flex-col items-center transition ease-in duration-200 hover:text-blue-400 ">
@@ -46,7 +46,7 @@
             </div>
             <div class="flex flex-col items-center  hover:text-blue-400 ">
                 <div
-                class="absolute bottom-5 shadow-lg text-center flex items-center justify-center rounded-full border-4 text-lg border-gray-50 hover:border-blue-500 bg-blue-400 w-20 h-20 p-2 text-white transition ease-in duration-200 ">
+                class="absolute bottom-5 shadow-2xl text-center flex items-center justify-center rounded-full border-4 text-xl border-gray-50 hover:border-blue-500 bg-blue-400 w-20 h-20 p-2 text-white transition ease-in duration-200 ">
                 +
                 <span class="animate-pingg absolute inline-flex h-full w-full rounded-full border-4 opacity-50"></span>
                 </div>
@@ -58,6 +58,5 @@
                 <i class="fa fa-user"></i>
             </div>
         </div>        
-
     </div>
 @endsection
