cbbe2e8abd90565d9ab495ef666d62bc355d811d
tjyusuf <tjyusuf@Yahoo.com>
2021-11-20 08:07:40 +0000

    service worker

--- resources/views/vendor/laravelpwa/meta.blade.php
+++ resources/views/vendor/laravelpwa/meta.blade.php
@@ -36,7 +36,7 @@
     // Initialize the service worker
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/serviceworker.js', {
-            scope: '.'
+            scope: '/'
         }).then(function (registration) {
             // Registration was successful
             console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
