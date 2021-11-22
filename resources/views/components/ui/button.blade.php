fe320b000830dd1bcc098a2e6bddd6d98b815f9e
tjyusuf <tjyusuf@Yahoo.com>
2021-11-22 22:51:57 +0000

    update

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,3 +1,5 @@
 <div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+    <button {{ $attributes->merge(['class' => 'justify-center px-4 py-2 text-sm font-medium text-white bg-primary border border-transparent rounded-md hover:bg-primary focus:outline-none focus:border-primary-dark focus:ring-primary active:bg-primary-dark transition duration-150 ease-in-out', 'value' => 'submit']) }} >
+
+    </button>
 </div>
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -67,9 +67,9 @@
 
                 <div class="mt-6">
                     <span class="block w-full rounded-md shadow-sm">
-                        <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                        <x-ui.button type="submit" class="flex w-full">
                             Sign in
-                        </button>
+                        </x-ui.button>
                     </span>
                 </div>
             </form>
