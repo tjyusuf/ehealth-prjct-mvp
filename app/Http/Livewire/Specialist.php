8f965bc3115832ebcb50ae42cb958e7ff35f08e2
Ubuntu <ubuntu@ip-172-31-45-26.us-east-2.compute.internal>
2021-11-22 16:04:13 +0000

    specialty and facility liuvewire component

---
+++ app/Http/Livewire/Specialist.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Specialist extends Component
+{
+    public function render()
+    {
+        return view('livewire.specialist');
+    }
+}
