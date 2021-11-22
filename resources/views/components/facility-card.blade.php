34ef481c4de5757d9cf200c6229159b85b218d96
tjyusuf <tjyusuf@Yahoo.com>
2021-11-22 19:22:00 +0000

    updates

--- resources/views/components/facility-card.blade.php
+++ resources/views/components/facility-card.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="border border-gray-light rounded-md w-full mb-2 bg-white shadow">
-        <div class="p-5 pb-2" >
+        <div class="p-2" >
           <h5 class="text-gray mb-2">
             <span class="font-semibold ">Barau Dikko</span>
           </h5>
@@ -17,8 +17,8 @@
           </h5>
         </div>
         <div class="flex border-t border-gray-light divide-x divide-gray">
-          <a class="flex-1 py-3 px-5 font-light cursor-pointer" >
-              <span class="bg-primary-light text-primary text-xs px-2 py-1 rounded">hospital</span>
+          <a class="flex-1 p-2 font-light cursor-pointer" >
+              <span class="bg-primary-light text-primary-dark font-semibold text-xs px-2 py-1 rounded">hospital</span>
               <span class="float-right">
               <i class="fa fa-chevron-right"></i>
             </span>
