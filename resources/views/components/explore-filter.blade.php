29f832817f98137f7b932d691d1c4ec9a8952cae
tjyusuf <tjyusuf@Yahoo.com>
2021-11-22 16:42:23 +0000

    some updates

--- resources/views/components/explore-filter.blade.php
+++ resources/views/components/explore-filter.blade.php
@@ -5,15 +5,15 @@
       <p  ><i class="fa fa-filter text-primary"></i></p>  
     </div>
     <div class="px-5 py-2 flex-col bg-gray-light" >
-      <div class="space-x-2 ">
-        <span class="px-4 py-2 text-base rounded-full text-white bg-primary">
+      <div class="space-x-2 flex">
+        <span class="flex-inline px-4 py-2 text-base rounded-full text-white bg-primary">
           <i class="fa fa-user-md mr-3"></i>
           Specialists
           <button class="bg-transparent hover">
             <i class="fas fa-times ml-3"></i>
           </button>
         </span>
-        <span class="px-4 py-2 text-base rounded-full bg-white text-primary">
+        <span class="flex-inline px-4 py-2 text-base rounded-full bg-white text-primary">
           <i class="fa fa-hospital mr-3"></i>
           Facilities
           <button class="bg-transparent hover">
