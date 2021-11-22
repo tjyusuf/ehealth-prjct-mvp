d769ca8c85bbcb7b60e6025351ba50ec22236733
tjyusuf <tjyusuf@Yahoo.com>
2021-11-22 12:28:01 +0000

    updates

--- resources/views/components/home-explore.blade.php
+++ resources/views/components/home-explore.blade.php
@@ -2,33 +2,43 @@
     <div class="flex w-full bg-white border p-4 space-x-4 rounded-lg">
         <i class="fa fa-search"></i>
         <input class="w-full bg-white outline-none" type="text" placeholder="Professionals, facilities, specialists..." />
+    
     </div>  
+
+    <div class="relative flex w-full flex-wrap items-stretch">
+        <span class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+          <i class="fas fa-search"></i>
+        </span>
+        <input type="text" placeholder="Search"
+          class="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border border-gray-400 outline-none focus:outline-none focus:ring w-full pl-10"/>
+    </div>
+
     <div class="py-3 space-y-4 z-0">
         <div class="flex items-center justify-between md:justify-center text-gray-500 cursor-pointer space-x-6">
             <div class="flex flex-col text-center text-gray-500 space-y-2">
-                <div class="flex items-center shadow-md justify-center w-20 h-20  rounded-lg bg-white">
-                <i class="fa fa-user-md text-blue-400 text-4xl"></i>
+            <div class="flex items-center shadow-md justify-center px-3 py-5 rounded-lg bg-white">
+                    <i class="fa fa-user-md text-blue-400 text-3xl"></i>
                 </div>          
                 <p>Specialists</p>
             </div>
             <div class="flex flex-col text-center text-gray-500 space-y-2">
-                <div class="flex items-center shadow-md justify-center w-20 h-20  rounded-lg bg-white">
-                <i class="fa fa-hospital text-blue-400 text-4xl"></i>
+            <div class="flex items-center shadow-md justify-center px-3 py-5 rounded-lg bg-white">
+                    <i class="fa fa-hospital text-blue-400 text-3xl"></i>
                 </div>          
                 <p>Facilities</p>
             </div>
             <div class="flex flex-col text-center text-gray-500 space-y-2">
-                <div class="flex items-center shadow-md justify-center w-20 h-20  rounded-lg bg-white">
-                <i class="fa fa-medkit text-blue-400 text-4xl"></i>
+                <div class="flex items-center shadow-md justify-center px-3 py-5 rounded-lg bg-white">
+                    <i class="fa fa-medkit text-blue-400 text-3xl"></i>
                 </div>          
                 <p>Prescrip..</p>
             </div>
-            <div class="flex flex-col text-center text-gray-500 space-y-2">
+            <!-- <div class="flex flex-col text-center text-gray-500 space-y-2">
                 <div class="flex items-center shadow-md justify-center w-20 h-20  rounded-lg bg-white">
                 <i class="fa fa-cart-plus text-blue-400 text-4xl"></i>
                 </div>          
                 <p>Orders</p>
-            </div>
+            </div> -->
         </div>
     </div>
       
