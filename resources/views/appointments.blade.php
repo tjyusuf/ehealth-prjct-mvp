d263a0674bbf0e02e98e496154850a9aba815ec0
tjyusuf <tjyusuf@Yahoo.com>
2021-11-23 10:23:28 +0000

    update

--- resources/views/appointments.blade.php
+++ resources/views/appointments.blade.php
@@ -4,12 +4,15 @@
     <div class="max-w-7xl mx-auto p-5 md:p-10 space-y-2">
         
         <div>
-            <p class="text-primary-dark mb-2 text-md font-bold">Upcoming appointment</p>
+            <p class="text-primary-dark mb-2 text-md font-bold">Upcoming appointments</p>
+            <x-appointments-upcoming-card/>
             <x-appointments-upcoming-card/>
         </div>
         <div>
-            <p class="text-primary-dark mb-2 text-md font-bold">Upcoming appointment</p>
-            <x-appointments-upcoming-card/>
+            <p class="text-primary-dark mb-2 text-md font-bold">Completed appointments</p>
+            <x-appointments-completed-card/>
+            <x-appointments-completed-card/>
+            <x-appointments-completed-card/>
         </div>
     </div>
 @endsection
