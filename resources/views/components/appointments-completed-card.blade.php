8f146cd4ce961d871df693db5c067ccf191b0c65
tjyusuf <tjyusuf@Yahoo.com>
2021-11-26 07:46:51 +0000

    update

--- resources/views/components/appointments-completed-card.blade.php
+++ resources/views/components/appointments-completed-card.blade.php
@@ -1,19 +1,24 @@
 <div>
     <div class="w-full rounded-md text-gray px-2 py-3 bg-white shadow-md">
-        <div class="flex flex-row">
-          <h1 class="flex-grow">
-            <p class="text-xl font-bold ">Dr Ahmad Mahmud</p>
-            <p>Consultant</p>
-          </h1>      
-          <div class="flex-initial">
-            <p class="flex-1 text-center">
-                <i class="fa fa-clock"></i> 5:30-6:00pm
-            </p>            
-            <p class="flex-1 text-center">
-                <i class="fa fa-calendar"></i> Wed, 17th Nov
-            </p>
+        <div class="flex-col">
+          <div class="flex flex-row">
+            <h1 class="flex-grow">
+              <p class="text-xl font-bold ">Dr Ahmad Mahmud</p>
+              <p>Consultant</p>
+            </h1>   
           </div>  
+          
+          <div class="flex flex-row py-2 px-3 rounded-lg text-sm space-x-2">
+            <p class="flex-1 text-right">
+              <i class="fa fa-calendar"></i> Wed, 17th Nov
+            </p>
+            <p class="flex-1 text-left">
+              <i class="fa fa-clock"></i> 5:30-6:00pm
+            </p>
+          </div> 
+          
         </div>  
+        
         <hr class="border-primary-light my-1"/>
         <div class="flex flex-row px-2 justify-between">
             <span class="text-primary">Completed</span>
--- resources/views/components/appointments-upcoming-card.blade.php
+++ resources/views/components/appointments-upcoming-card.blade.php
@@ -1,22 +1,25 @@
 <div>
     <div class="w-full rounded-md text-gray px-3 py-5 bg-white shadow-md">
-        <div class="flex flex-row">
-          <div class="flex-initial">
-            <img alt="" class="w-12 h-12 rounded-full ring-2 ring-offset-2 ring-primary ring-offset-primary-light" src="https://source.unsplash.com/41x41/?portrait"/>
+        <div class="flex flex-col">
+          <div class="flex flex-row">
+            <div class="flex-initial">
+              <img alt="" class="w-12 h-12 rounded-full ring-2 ring-offset-2 ring-primary ring-offset-primary-light" src="https://source.unsplash.com/41x41/?portrait"/>
+            </div>           
+            <h1 class="ml-5 flex-grow">
+              <p class="text-xl font-bold ">Dr Sani Ahmad</p>
+              <p>Dentist</p>
+            </h1>      
           </div>           
-          <h1 class="ml-5 flex-grow">
-            <p class="text-xl font-bold ">Dr Sani Ahmad</p>
-            <p>Dentist</p>
-          </h1>      
-          <div class="flex-initial">
-            <p class="flex-1 text-center">
-                <i class="fa fa-clock"></i> 5:30-6:00pm
-            </p>            
-            <p class="flex-1 text-center">
-                <i class="fa fa-calendar"></i> Wed, 17th Nov
-            </p>
 
-          </div>  
+          <div class="flex flex-row py-2 px-3 rounded-lg text-sm space-x-2">
+            <p class="flex-1 text-right">
+              <i class="fa fa-calendar"></i> Wed, 17th Nov
+            </p>
+            <p class="flex-1 text-left">
+              <i class="fa fa-clock"></i> 5:30-6:00pm
+            </p>
+          </div> 
+
         </div>  
         <hr class="border-primary-light my-2"/>
         <div class="flex flex-row px-3 justify-between">
