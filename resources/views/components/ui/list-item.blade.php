9a178af3e4a3ba05a9ed9f5b8e9705b1242769ee
Tijjani <tjyusuf@Yahoo.com>
2021-11-28 10:34:35 +0100

    Update list-item.blade.php

--- resources/views/components/ui/list-item.blade.php
+++ resources/views/components/ui/list-item.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+<div {!! $attributes->merge(['class' => 'bg-white rounded shadow flex py-2 px-3 my-1 items-center text-gray']) !!}>
+	@if(isset($icon))
+        <p class="flex-inline flex-shrink-0 mr-4">
+            <i class="fa fa-$icon text-base"></i>
+        </p>
+	@endif    
+	<div class="flex-inline w-full">
+		<p class="flex text-base font-bold">{{ $title }}</p>
+		<p class="flex text-xs md:text-sm text-gray w-full">
+			{{(isset($content)) ? $content : '' }}
+		</p>
+	</div>
+	@if(isset($more))
+		<p class="flex-inline text-primary">
+			{{ $more }}	
+		</p>
+	@endif
 </div>
