8b8c932ec195892b842d3f5ec69a57b1eafe21df
tijjvni <tijjvni@gmail.com>
2021-12-13 09:27:21 +0000

    re-lb config

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::group(function(){
+// Route::group(function(){
+Route::middleware('auth:sanctum')->group(function(){
 
     Route::get('/user', function (Request $request) {
         return $request->user();
