22ca98bb2b9a9811355bc1f49eddd2dca20700a0
Ubuntu <ubuntu@ip-172-31-28-184.us-east-2.compute.internal>
2021-12-14 17:16:24 +0000

    new branch

---
+++ app/Http/Livewire/Openings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Openings extends Component
+{
+    public function render()
+    {
+        return view('livewire.openings');
+    }
+}
