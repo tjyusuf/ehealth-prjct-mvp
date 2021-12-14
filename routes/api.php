bfac217507f4cbbd2a34ebb34fd7ca389f9a42d0
tijjvni <tijjvni@gmail.com>
2021-12-14 15:12:11 +0000

    updte

--- routes/api.php
+++ routes/api.php
@@ -4,14 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RatingsController;
+use App\Http\Controllers\API\AuthController;
 
-Route::get('/user',[ RatingsController::class, 'index']);
+Route::post('/auth/register', [AuthController::class, 'register']);
 
-// // Route::group(function(){
-// Route::middleware('auth:sanctum')->group(function(){
+Route::post('/auth/login', [AuthController::class, 'login']);
 
-//     Route::get('/user', function (Request $request) {
-//         return $request->user();
-//     })->name('user');
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/me', function(Request $request) {
+        return auth()->user();
+    });
 
-// });
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+});
