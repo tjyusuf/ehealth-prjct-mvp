6e07ea485c4c97afe330781102c349605c35beb5
tijjvni <tijjvni@gmail.com>
2021-12-14 18:07:30 +0000

    controllerS

--- app/Http/Controllers/API/SpecialistsController.php
+++ app/Http/Controllers/API/SpecialistsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Http\Traits\ApiResponse;
+use App\Traits\ApiResponse;
 
 class SpecialistsController extends Controller
 {
